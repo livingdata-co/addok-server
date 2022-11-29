34374104f13090c39003605f44b645ec8c1b77ea
Jérôme Desboeufs <jerome@livingdata.co>
2022-11-29 16:32:15 +0100

    Add /batch route

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import process from 'node:process'
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import createError from 'http-errors'
 
 import {createCluster} from 'addok-cluster'
 
@@ -51,6 +52,65 @@ app.get('/search', w(async (req, res) => {
     center: params.lon && params.lat ? [params.lon, params.lat] : undefined,
     limit: params.limit
   })
+}))
+
+app.post('/batch', express.json(), w(async (req, res) => {
+  const {requests} = req.body
+
+  if (!Array.isArray(requests)) {
+    throw createError(400, 'requests is a required param (array)')
+  }
+
+  if (requests.length > 100) {
+    throw createError(400, 'requests must not contains more than 100 items')
+  }
+
+  for (const r of requests) {
+    if (!['geocode', 'reverse'].includes(r.operation)) {
+      throw createError(400, 'operation must be one of geocode or reverse')
+    }
+
+    if (!r.params) {
+      throw createError(400, 'params is required for each requests item')
+    }
+  }
+
+  const baseParams = {limit: 2, autocomplete: false}
+  const globalParams = req.body.params ? {...req.body.params, ...baseParams} : baseParams
+
+  const results = await Promise.all(requests.map(async r => {
+    try {
+      const {operation, params} = r
+      const rParams = {...params, ...globalParams}
+      const operationResult = await cluster[operation](rParams)
+
+      if (operationResult.length === 0) {
+        return {
+          status: 'not-found'
+        }
+      }
+
+      const result = {
+        ...operationResult[0].properties,
+        next_score: operationResult[1] ? operationResult[1].properties.score : undefined,
+        lon: operationResult[0].geometry.coordinates[0],
+        lat: operationResult[0].geometry.coordinates[1]
+      }
+
+      return {
+        status: 'ok',
+        result
+      }
+    } catch (error) {
+      console.error(error)
+      return {
+        status: 'error',
+        error: error.message
+      }
+    }
+  }))
+
+  res.send({results})
 }))
 
 app.use(errorHandler)
