369936be9cd4cac76b09939b4fafaabe3ee770d4
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-03-10 19:39:39 +0400

    Move routes from server.js to lib/routes.js

--- server.js
+++ server.js
@@ -4,22 +4,15 @@ import 'dotenv/config.js'
 import process from 'node:process'
 
 import express from 'express'
-import multer from 'multer'
 import cors from 'cors'
 import morgan from 'morgan'
-
 import {createCluster} from 'addok-cluster'
 
-import {search} from './lib/search.js'
-import {batch} from './lib/batch.js'
-import {csv} from './lib/csv.js'
-import w from './lib/w.js'
-import errorHandler from './lib/error-handler.js'
+import routes from './lib/routes.js'
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
-const upload = multer()
 const cluster = await createCluster()
 
 app.disable('x-powered-by')
@@ -30,15 +23,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 app.use(cors({origin: true}))
 
-app.get('/search', w(search({cluster})))
-app.get('/reverse', w(search({cluster, reverse: true})))
-
-app.post('/batch', express.json(), w(batch({cluster})))
-
-app.post('/search/csv', upload.single('data'), w(csv({cluster})))
-app.post('/reverse/csv', upload.single('data'), w(csv({cluster, reverse: true})))
-
-app.use(errorHandler)
+app.use('/', routes(cluster))
 
 app.listen(PORT, () => {
   console.log(`Start listening on port ${PORT}`)
