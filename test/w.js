455e22b400d6980de4fb5a47afbf425542ea7456
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-03-17 12:09:22 +0400

    Test 'errorHandler' and 'w'

---
+++ test/w.js
@@ -0,0 +1,24 @@
+import test from 'ava'
+import express from 'express'
+import request from 'supertest'
+import w from '../lib/w.js'
+
+test('w with a simple 200 response', async t => {
+  const app = express()
+
+  app.get('/', w(async (req, res) => res.sendStatus(200)))
+
+  const response = await request(app).get('/')
+  t.is(response.status, 200)
+})
+
+test('w with an error thown', async t => {
+  const app = express()
+
+  app.get('/', w(async () => {
+    throw new Error('Error')
+  }))
+
+  const response = await request(app).get('/')
+  t.is(response.status, 500)
+})
